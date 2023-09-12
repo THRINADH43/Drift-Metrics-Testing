1e6c54568db7da0e4387630ec9ee7171e8189e82
Thrinadh Manubothu <m.thrinadhreddy@gmail.com>
2023-09-12 08:12:34 +0530

    Files to testing the integration with pureml_monitor

---
+++ pureml_monitor_1/predict.py
@@ -0,0 +1,17 @@
+from pureml import BasePredictor,Input,Output
+import pureml
+
+
+class Predictor(BasePredictor):
+    label = 'model_monitoring:main:v1'
+    input = Input(type = "pandas dataframe")
+    output = Output(type = "numpy ndarray")
+
+    def load_models(self):
+        self.model = pureml.model.fetch(self.label)
+
+    def predict(self, data):
+        predictions  = self.model.predict(data)
+
+        return predictions
+    
